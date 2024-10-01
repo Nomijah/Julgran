b86c6313a11883eba06fe68896a01c81b0f742af
Nomijah <petter.bostrom@gmail.com>
2024-10-01 21:52:24 +0200

    Added trunk and adjusted printing of star and tree

--- Program.cs
+++ Program.cs
@@ -5,6 +5,91 @@ namespace Ovning_Julgran
 {
     class Program
     {
+
+
+        static int treeSize;
+        static void Main(string[] args)
+        {
+            // Ask user to enter a number to determine size of tree.
+            Console.Write("Hur många rader hög ska granen vara? ");
+            string userInput = Console.ReadLine();
+            while (!Int32.TryParse(userInput, out treeSize))
+            {
+                Console.WriteLine("Använd endast siffror, försök igen:");
+                userInput = Console.ReadLine();
+            }
+            // Make an array containing odd numbers in the amount chosen by user.
+            int[] tree = new int[treeSize];
+            int counter = 1;
+            for (int i = 0; i < treeSize; i++)
+            {
+                tree[i] = counter;
+                counter += 2;
+            }
+
+            Console.WriteLine();
+            // Print the star.
+            if (treeSize > 15)
+            {
+                StarPrint(tree[treeSize - 1]); // tree[treeSize - 1] gets the width of the largest part of the tree
+            }
+            // Print the tree
+            PrintTree(tree);
+            PrintTrunk(tree[treeSize - 1]);
+
+            Console.ReadKey();
+        }
+
+        static void PrintTree(int[] tree)
+        {
+            // Variables for controlling that there is distance between balls.
+            int ballCounter = 0;
+            bool tooMany = false;
+            var rnd = new Random();
+            int resize = 0;
+            for (int i = 0; i < treeSize; i++)
+            {
+                // Adjust size of tree after set amount of rows
+                if (i == Math.Round(((decimal)treeSize / 20) * 3) && treeSize > 15 ||
+                    i == Math.Round(((decimal)treeSize / 20) * 7) && treeSize > 10 ||
+                    i == Math.Round(((decimal)treeSize / 20) * 12) && treeSize > 4)
+                {
+                    resize += 4;
+                }
+
+                for (int j = 0; j < (tree[treeSize - 1] - (tree[i] - resize)) / 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < (tree[i] - resize); k++)
+                {
+                    // Randomize ball placement on the tree.
+                    int ball = rnd.Next(4);
+                    if (ball == 3 && tooMany == false)
+                    {
+                        BallPrint();
+                        // Set boolean to true to avoid entering if-statement next round.
+                        tooMany = true;
+                        // Reset counter.
+                        ballCounter = 0;
+                    }
+                    else
+                    {
+                        TreePrint();
+                        ballCounter++;
+                    }
+                    // When ball has not been placed for 4 rounds, enter random-loop again.
+                    if (ballCounter == 4)
+                    {
+                        tooMany = false;
+                    }
+                }
+
+                // Go to next row.
+                Console.Write("\n");
+            }
+        }
+
         // Method for printing ball with random color.
         static void BallPrint()
         {
@@ -90,160 +175,48 @@ namespace Ovning_Julgran
             Console.WriteLine();
         }
 
-        static int userNumber;
-        static void Main(string[] args)
+        // Method for printing tree trunk
+        static void PrintTrunk(int size)
         {
-            // Ask user to enter a number to determine size of tree.
-            Console.Write("Hur många rader hög ska granen vara? ");
-            string userInput = Console.ReadLine();
-            while (!Int32.TryParse(userInput, out userNumber))
-            {
-                Console.WriteLine("Använd endast siffror, försök igen:");
-                userInput = Console.ReadLine();
-            }
-            // Make an array containing odd numbers in the amount chosen by user.
-            int[] tree = new int[userNumber];
-            int counter = 1;
-            for (int i = 0; i < userNumber; i++)
-            {
-                tree[i] = counter;
-                counter += 2;
-            }
-
-            // Print the star.
-            Console.WriteLine();
-            StarPrint(tree[userNumber - 1]);
-            // Print the tree
-            PrintTree(tree);
-
-            Console.ReadKey();
-        }
-
-        static void PrintTree(int[] tree)
-        {
-            // Variables for controlling that there is distance between balls.
-            int ballCounter = 0;
-            bool tooMany = false;
+            // Using size to determine the center of the tree.
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            char[] trunkDetails = ['/', 'W', 'X', '\\', 'Z'];
             var rnd = new Random();
-            for (int i = 0; i < userNumber; i++)
+            int trunkSize;
+            switch (treeSize)
             {
-                if (i < 4)
+                case < 11:
+                    trunkSize = 3;
+                    break;
+                case > 10 and < 18:
+                    trunkSize = 5;
+                    break;
+                case > 17:
+                    trunkSize = 7;
+                    break;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < (size - trunkSize) / 2; j++)
                 {
-                    for (int j = 0; j < (tree[userNumber - 1] - tree[i]) / 2; j++)
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < trunkSize; k++)
+                {
+                    if (k == 0)
                     {
-                        Console.Write(" ");
+                        Console.Write("|");
                     }
-                    for (int k = 0; k < tree[i]; k++)
+                    else if (k < trunkSize - 1)
                     {
-                        // Randomize ball placement on the tree.
-                        int ball = rnd.Next(4);
-                        if (ball == 3 && tooMany == false)
-                        {
-                            BallPrint();
-                            // Set boolean to true to avoid entering if-statement next round.
-                            tooMany = true;
-                            // Reset counter.
-                            ballCounter = 0;
-                        }
-                        else
-                        {
-                            TreePrint();
-                            ballCounter++;
-                        }
-                        // When ball has not been placed for 4 rounds, enter random-loop again.
-                        if (ballCounter == 4)
-                        {
-                            tooMany = false;
-                        }
+                        Console.Write(trunkDetails[rnd.Next(0, 5)]);
+                    }
+                    else
+                    {
+                        Console.Write("|");
                     }
                 }
-
-                // Adjust size of tree after 4 rounds.
-                else if (i >= 4 && i < 9)
-                {
-                    for (int j = 0; j < (tree[userNumber - 1] - (tree[i] - 4)) / 2; j++)
-                    {
-                        Console.Write(" ");
-                    }
-                    for (int k = 0; k < (tree[i] - 4); k++)
-                    {
-                        int ball = rnd.Next(4);
-                        if (ball == 3 && tooMany == false)
-                        {
-                            BallPrint();
-                            tooMany = true;
-                            ballCounter = 0;
-                        }
-                        else
-                        {
-                            TreePrint();
-                            ballCounter++;
-                        }
-                        if (ballCounter == 4)
-                        {
-                            tooMany = false;
-                        }
-                    }
-                }
-
-                // Adjust size of tree again.
-                else if (i >= 9 && i < 17)
-                {
-                    for (int j = 0; j < (tree[userNumber - 1] - (tree[i] - 8)) / 2; j++)
-                    {
-                        Console.Write(" ");
-                    }
-                    for (int k = 0; k < (tree[i] - 8); k++)
-                    {
-                        int ball = rnd.Next(4);
-                        if (ball == 3 && tooMany == false)
-                        {
-                            BallPrint();
-                            tooMany = true;
-                            ballCounter = 0;
-                        }
-                        else
-                        {
-                            TreePrint();
-                            ballCounter++;
-                        }
-                        if (ballCounter == 4)
-                        {
-                            tooMany = false;
-                        }
-                    }
-                }
-
-                // Adjust size of tree one last time.
-                else if (i >= 17)
-                {
-                    for (int j = 0; j < (tree[userNumber - 1] - (tree[i] - 12)) / 2; j++)
-                    {
-                        Console.Write(" ");
-                    }
-                    for (int k = 0; k < (tree[i] - 12); k++)
-                    {
-                        int ball = rnd.Next(4);
-                        if (ball == 3 && tooMany == false)
-                        {
-                            BallPrint();
-                            tooMany = true;
-                            ballCounter = 0;
-                        }
-                        else
-                        {
-                            TreePrint();
-                            ballCounter++;
-                        }
-                        if (ballCounter == 4)
-                        {
-                            tooMany = false;
-                        }
-                    }
-                }
-
-                // Go to next row.
-                Console.Write("\n");
+                Console.WriteLine();
             }
         }
     }
